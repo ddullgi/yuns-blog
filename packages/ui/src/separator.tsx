91d1d01f745e990cefa64b0561e67af580197c74
황상윤 <s_infinite@naver.com>
2024-10-06 23:15:46 +0900

    [feat:ui-package] class-variance-authority 추가 & separator/spacing 컴포넌트 수정

--- packages/ui/src/separator.tsx
+++ packages/ui/src/separator.tsx
@@ -5,6 +5,11 @@ interface SeparatorProps {
 	className?: string;
 }
 
+/**
+ * 분할 선을 만드는 컴포넌트(tailwind className에 맞춰 입력)
+ * @param { "horizontal" | "vertical" } orientation - 방향 horizontal | vertical
+ * @param { string } className - 추가적으로 넣을 className
+ */
 export const Separator = ({
 	orientation = "horizontal",
 	className = "",
--- packages/ui/src/spacing.tsx
+++ packages/ui/src/spacing.tsx
@@ -1,10 +1,14 @@
+import type { VariantProps } from "class-variance-authority";
 import { cn } from "./cn";
+import { stackVariants } from "./variants/stack-variants";
 
-interface SpacingProps {
-	w?: string;
-	h?: string;
-}
+type SpacingProps = Pick<VariantProps<typeof stackVariants>, "w" | "h">;
 
-export const Spacing = ({ w = "", h = "" }: SpacingProps) => {
-	return <div className={cn(w, h)} />;
+/**
+ * 빈공간을 만들어주는 컴포넌트(tailwind className에 맞춰 입력)
+ * @param { string } w - 공간의 넓이
+ * @param { string } h - 공간의 높이
+ */
+export const Spacing = ({ w, h }: SpacingProps) => {
+	return <div className={cn(stackVariants({ w, h }))} />;
 };
