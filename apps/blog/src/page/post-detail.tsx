c585a727c46142cdccc3aeb3c3bdf7528b7a478d
황상윤 <s_infinite@naver.com>
2024-10-14 23:10:39 +0900

    [feat:blog] post-detail-title 컴포넌트 추가

--- apps/blog/src/page/post-detail.tsx
+++ apps/blog/src/page/post-detail.tsx
@@ -1,5 +1,6 @@
 import type { PostWithFrontmatterType } from "@/entities/post/model/post.type";
 import { MdxRemote } from "@/entities/post/ui/mdx/mdx-remote";
+import { PostDetailTitle } from "@/entities/post/ui/post/post-detail-title";
 import { cn } from "@/shared/utils/cn";
 import { Separator } from "@yuns-blog/ui/separator";
 
@@ -10,6 +11,7 @@ type PostPageProps = {
 export default function PostDetailPage({ post }: PostPageProps) {
 	return (
 		<section className={cn("max-w-2xl")}>
+			<PostDetailTitle>{post.title}</PostDetailTitle>
 			<MdxRemote source={post.content} />
 			<Separator />
 			<div>123</div>
