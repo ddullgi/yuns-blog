01811babed13bab2d254e5b765c0661d66887955
황상윤 <s_infinite@naver.com>
2024-10-31 23:22:59 +0900

    [study:next] pages 라우터 기본 구성 공부

---
+++ apps/study/src/pages/404.tsx
@@ -0,0 +1,15 @@
+import { useCallback } from "react";
+
+export default function My404Page() {
+	const handleClick = useCallback(() => {
+		console.log("hi"); // eslint-disable-line no-console
+	}, []);
+	return (
+		<h1>
+			페이지를 찾을 수 없습니다.{" "}
+			<button type="button" onClick={handleClick}>
+				클릭
+			</button>
+		</h1>
+	);
+}
---
+++ apps/study/src/pages/_error.tsx
@@ -0,0 +1,18 @@
+import type { NextPageContext } from "next";
+
+// biome-ignore lint/suspicious/noShadowRestrictedNames: <explanation>
+function Error({ statusCode }: { statusCode: number }) {
+	return (
+		<>
+			{statusCode ? `서버에서 ${statusCode}` : "클라이언트에서"} 에러가
+			발생했습니다.
+		</>
+	);
+}
+
+Error.getInitialProps = ({ res, err }: NextPageContext) => {
+	const statusCode = res ? res.statusCode : err ? err.statusCode : "";
+	return { statusCode };
+};
+
+export default Error;
